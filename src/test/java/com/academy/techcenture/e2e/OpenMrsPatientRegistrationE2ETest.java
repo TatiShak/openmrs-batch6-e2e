ac951dbc505dc3bbb7704648ec2973d12aa53070
SulamitaMusik <sulamitamusaeva9@gmail.com>
2022-06-09 22:38:51 -0400

    Sulamitas changes register in progrss

--- src/test/java/com/academy/techcenture/e2e/OpenMrsPatientRegistrationE2ETest.java
+++ src/test/java/com/academy/techcenture/e2e/OpenMrsPatientRegistrationE2ETest.java
@@ -5,9 +5,12 @@ import com.academy.techcenture.config.Driver;
 import com.academy.techcenture.pages.HomePage;
 import com.academy.techcenture.pages.LoginPage;
 import com.academy.techcenture.pages.RegisterPage;
+import com.academy.techcenture.utils.ExcelReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -21,11 +24,12 @@ public class OpenMrsPatientRegistrationE2ETest {
     @BeforeMethod
     public void driverSetup(){
         driver = Driver.getDriver();
+        driver.manage().window().maximize();
         driver.get(ConfigReader.getProperty("URL"));
         softAssert = new SoftAssert();
     }
 
-    @Test
+    @Test(dataProvider = "newPatientData")
     public void patientRegisterPositive(Map<String, String> patientDetails){
         LoginPage loginPage = new LoginPage(driver,softAssert);
         HomePage homePage = new HomePage(driver, softAssert);
@@ -33,7 +37,27 @@ public class OpenMrsPatientRegistrationE2ETest {
         loginPage.login();
         homePage.clickOnRegisterPatientTab();
         registerPage.register(patientDetails);
+
+        softAssert.assertAll();
+
     }
+
+    @DataProvider(name = "newPatientData")
+    public Object[][] getNewUsersData() {
+        ExcelReader excelReader = new ExcelReader("src/main/resources/testData/patientData.xlsx", "patientDB");
+        return excelReader.getData();
+    }
+
+
+
+    @AfterMethod
+    public void cleanUp() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
 
 
 }
